6fa7ae28323150997bddf19e1f65d988de00c548
hosais <hosais@gmail.com>
2020-08-17 17:25:33 +0800

    define spreadsheet tables, add and test time date module, use tk as UI module

--- worktime.py
+++ worktime.py
@@ -1,16 +1,44 @@
+from datetime import datetime
+
+from openpyxl import Workbook
+
+
+
+
+
+
+
+
+
+
 def checkPW():
+    
+    workbook = Workbook()
+    sheet = workbook.active
+
+    now = datetime.now()
+    date = now.today()
+    current_time = now.strftime("%m/%d -- %H:%M:%S")
+    #print("Current Time =", current_time)
+    #https://stackoverflow.com/questions/50491839/python-openpyxl-find-strings-in-column-and-return-row-number
     if(pw.get()=="01"):
-	    msg.set("張三歡迎登入,您簽到的時間是:"+"")
-	elif(pw.get()=="02"):
-        msg.set("李四歡迎登入,您簽到的時間是:"+"")
+        msg.set("張三歡迎登入,您簽到的時間是:"+current_time)
+        sheet["A1"] = "張三01"
+        sheet["B1"] = now
+    elif(pw.get()=="02"):
+        msg.set("李四歡迎登入,您簽到的時間是:"+current_time)
     elif(pw.get()=="03"):
-        msg.set("王五歡迎登入,您簽到的時間是:"=+"")
+        msg.set("王五歡迎登入,您簽到的時間是:"+current_time)
     elif(pw.get()=="04"):
-        msg.set("陳六歡迎登入,您簽到的時間是:"+"")
+        msg.set("陳六歡迎登入,您簽到的時間是:"+current_time)
     else:
         msg.set("您輸入錯誤工號,請重新輸入!")
+        
+        
+    workbook.save(filename="hello_world.xlsx")
 		
 import tkinter as tk
+
 
 worktime=tk.Tk()
 pw=tk.StringVar()
@@ -26,3 +54,5 @@ button.pack()
 lblmsg=tk.Label(worktime,fg="red",textvariable=msg)
 lblmsg.pack()
 worktime.mainloop()
+
+
