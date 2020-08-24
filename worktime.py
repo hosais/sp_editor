98e9f0d2b7f17b0b41b9a11b512070fdf1ad1758
hosais <hosais@gmail.com>
2020-08-25 00:05:42 +0800

    add spreadsheet search functions. add finding user name func to check_pw()

--- worktime.py
+++ worktime.py
@@ -1,30 +1,58 @@
 from datetime import datetime
 
 from openpyxl import Workbook
+from openpyxl import load_workbook
 
 
-
-
-
-
-
-
-
-
-
-    
     
 		
 import tkinter as tk
 
+#################################################################3
 
-    
+# Openpyxl searching data functions
+# reference: 
+# get worksheet by name: https://stackoverflow.com/questions/36814050/openpyxl-get-sheet-by-name
+
+# code source: https://stackoverflow.com/questions/50491839/python-openpyxl-find-strings-in-column-and-return-row-number
+# wb = load_workbook(filename = 'empty_book.xlsx')
+# ws = wb.active
+# col_idx, row = search_value_in_col_index(ws, "_my_search_string_")
+
+def search_value_in_column(ws, search_string, column="A"):    
+    for row in range(1, ws.max_row + 1):
+        coordinate = "{}{}".format(column, row)        
+        if ws[coordinate].value == search_string:             
+            return column, row   
+    return column, None
 
 
+def search_value_in_col_idx(ws, search_string, col_idx=1):
+    for row in range(1, ws.max_row + 1):
+        if ws[row][col_idx].value == search_string:
+            return col_idx, row
+    return col_idx, None
 
+
+def search_value_in_row_index(ws, search_string, row=1):
+    for cell in ws[row]:
+        if cell.value == search_string:
+            return cell.column, row
+    return None, row    
+
+
+#######################################3333
+# WorkTime
 
 class WorkTime:
-    def __init__(self):
+    def __init__(self, filename="hello_world.xlsx"):
+        # Openpyxl
+        self.data_filename = filename
+        self.worker_table_name="workers"
+        self.data_wb = load_workbook(filename = self.data_filename)
+        
+        
+        #UI
         self.worktime_ui = tk.Tk()
         self.employee_num=tk.StringVar()
         self.msg=tk.StringVar()
@@ -39,12 +67,21 @@ class WorkTime:
         self.result_msg=tk.Label(self.worktime_ui,fg="red",textvariable=self.msg)
         self.result_msg.pack()
         self.worktime_ui.mainloop()
-    def get_user_name(employee_num): 
+    def get_user_name(self, employee_num): 
         '''
             Open spreadsheet workers (ID, name) and search the matched user name
             return user name
             
-        '''
+        '''                
+        workers_ws = self.data_wb[self.worker_table_name]        
+        column, row = search_value_in_column(workers_ws, employee_num, column="A")
+              
+        if row == None: 
+            # not found
+            return None       
+        coordinate = "{}{}".format("B", row)        
+        return workers_ws[coordinate].value
+        
     def is_login_history_normal(self):
         '''
             exist 2 or times of 2 login records 
@@ -62,38 +99,30 @@ class WorkTime:
         '''
     def check_pw(self):
 
-        workbook = Workbook()
-        sheet = workbook.active
 
         now = datetime.now()
         date = now.today()
         current_time = now.strftime("%m/%d -- %H:%M:%S")
         #print("Current Time =", current_time)
         #https://stackoverflow.com/questions/50491839/python-openpyxl-find-strings-in-column-and-return-row-number
-        #
-       
-        if(self.employee_num.get()=="01"):
-            self.msg.set("張三歡迎登入,您簽到的時間是:"+current_time)
-            sheet["A1"] = "張三01"
-            sheet["B1"] = now
-        elif(self.employee_num.get()=="02"):
-            self.msg.set("李四歡迎登入,您簽到的時間是:"+current_time)
-        elif(self.employee_num.get()=="03"):
-            self.msg.set("王五歡迎登入,您簽到的時間是:"+current_time)
-        elif(self.employee_num.get()=="04"):
-            self.msg.set("陳六歡迎登入,您簽到的時間是:"+current_time)
-        else:
-            self.msg.set("您輸入錯誤工號,請重新輸入!")
+        user_id = self.employee_num.get()
+        user_name = self.get_user_name(user_id)
+        if user_name == None: 
+            user_name = "user not Found"        
+        msg_str = "歡迎 "+user_name+" 登入,您簽到的時間是:"+current_time
+        self.msg.set(msg_str)
+        
+        
 
 
-        workbook.save(filename="hello_world.xlsx")
+        #workbook.save(self.data_filename)
         
     
-
-
+######## End of work time
+##########################################
 def main():
     
-    wt = WorkTime()    
+    wt = WorkTime(filename="work_log.xlsx")    
 
     
     
